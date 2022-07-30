4f57a3a2c70cde2af3437cc9722a8c9f58fa8a62
VovaChaika <75807498+VovaChaika@users.noreply.github.com>
2022-07-30 22:30:38 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,9 @@ import s from "./Footer.module.css"
 const Footer = () => {
     return (
         <div className={s.footer}>
-            All rights reserved
+            Other my projects:
+            <span><a href="https://vovachaika.github.io/chess-game" target="_blank">React Chess</a></span><div></div>
+            <span><a href="https://currency-converter-two-dun.vercel.app/" target="_blank">Currency Converter</a></span>
         </div>
     );
 };
